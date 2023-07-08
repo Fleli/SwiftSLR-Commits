88b43a11e41df666fdb676823fc5b429bcb7d255
Fleli <frederikedvardsen@gmail.com>
2023-07-08 22:25:50 +0200

    Setup

---
+++ SwiftSLR/SLRNode.swift
@@ -0,0 +1,11 @@
+class SLRNode {
+    
+    let type: String
+    
+    var children: [SLRNode] = []
+    
+    init(type: String) {
+        self.type = type
+    }
+    
+}
--- SwiftSLR/main.swift
+++ SwiftSLR/main.swift
@@ -1,11 +1,1 @@
-//
-//  main.swift
-//  SwiftSLR
-//
-//  Created by Frederik Edvardsen on 08/07/2023.
-//
-
-import Foundation
-
-print("Hello, World!")
 
